7002c7a95dc49fe7bebb33270e924b6ddb44aecb
Amal S Raj <amalsraj95@gmail.com>
2023-10-13 19:54:13 +0530

    Implement watcher to watch file. Fully working.

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 import fs from 'fs'
 import { SaveData } from './src/savedata.js'
+import Watcher from 'watcher'
 
-const readFileSyncPS = performance.now()
-const sl2Buffer = fs.readFileSync('../tools-and-resources/ER0000-gutter.sl2')
-const readFileSyncPE = performance.now()
-console.log('readFileSync time = ' + (readFileSyncPE - readFileSyncPS) + 'ms')
+// const saveFilePath = '../tools-and-resources/ER0000-gutter.sl2'
+const saveFilePath = `C:/Users/Admin/AppData/Roaming/EldenRing/76561198079103089/ER0000.sl2`
 
-const getSaveDataPS = performance.now()
-const saveData = new SaveData(sl2Buffer)
-const getSaveDataPE = performance.now()
-console.log('getSaveData time = ' + (getSaveDataPE - getSaveDataPS) + 'ms')
+let deathCount = -1;
+console.clear();
 
-console.log(saveData);
+function printSaveInfo(slotId) {
+    console.time('readFileSync time')
+    const sl2Buffer = fs.readFileSync(saveFilePath)
+    console.timeEnd('readFileSync time')
+
+    console.time('saveData parse time')
+    const saveData = new SaveData(sl2Buffer)
+    console.timeEnd('saveData parse time')
+
+    console.log(saveData.characters[slotId]);
+    if (saveData.characters[slotId].deathCount != deathCount) {
+        deathCount = saveData.characters[slotId].deathCount;
+        console.clear();
+        console.log('ELDEN RING DEATH COUNTER');
+        console.log('Name: ', saveData.characters[slotId].name);
+        console.log('Deaths: ', deathCount);
+    }
+
+}
+
+const watcher = new Watcher(saveFilePath);
+watcher.on('ready', (e, t, n) => {
+    // console.log('Watcher initialized, ready to process events!');
+})
+watcher.on('change', (event, targetPath, targetPathNext) => {
+    printSaveInfo(1);
+})
+watcher.on('add', (event, targetPath, targetPathNext) => {
+    printSaveInfo(1);
+})
+
